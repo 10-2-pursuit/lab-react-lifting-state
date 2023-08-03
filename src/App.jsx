17cf9a36f5d5a5f6afb5ffc76d0be1f79e7f8a15
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-08-02 22:30:00 -0400

    still working, done w/ attandee

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import eventsData from "./data";
 import { v1 as generateUniqueID } from "uuid";
+import Header from "./Components/Header";
+import NewEventForm from "./Components/NewEventForm";
+import Footer from "./Components/Footer";
+import Attendee from "./Components/Attendee";
 // import Attendees from "./Attendees";
 // import Event from "./Components/Event";
 // import Footer from "./Components/Footer";
@@ -23,48 +27,6 @@ function App() {
     date: "",
     people: [],
   });
-
-  function addEvent() {
-    const createEvent = {
-      id: generateUniqueID(),
-      eventType: selectOption,
-      name: newEvent.name,
-      organizer: newEvent.organizer,
-      eventImage: newEvent.eventImage || "https://loremflickr.com/640/480/",
-      date: newEvent.date,
-      people: [],
-    };
-    handleAddEvent(createEvent);
-  }
-
-  function handleSelectChange(e) {
-    setSelectOption(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    addEvent();
-    resetEventForm();
-  }
-
-  function handleTextChange(e) {
-    setNewEvent({
-      ...newEvent,
-      [e.target.id]: e.target.value,
-    });
-  }
-
-  function resetEventForm() {
-    setNewEvent({
-      id: "",
-      eventType: "",
-      name: "",
-      organizer: "",
-      eventImage: "",
-      date: "",
-    });
-    setSelectOption("");
-  }
 
   function handleAddEvent(event) {
     setEvents([event, ...events]);
@@ -91,50 +53,12 @@ function App() {
   return (
     <div className="App">
       <>
-        <header>
-          <h1 className="color-change-5x">RSVP App</h1>
-        </header>
+        <Header />
       </>
       <main>
         <div className="new-event">
           <>
-            <form onSubmit={handleSubmit}>
-              <h3>Create a new event</h3>
-              <label htmlFor="name">Event name:</label>
-              <input
-                type="text"
-                id="name"
-                onChange={handleTextChange}
-                value={newEvent.name}
-              />
-
-              <label htmlFor="organizer">Organizer:</label>
-              <input
-                type="text"
-                id="organizer"
-                onChange={handleTextChange}
-                value={newEvent.organizer}
-              />
-
-              <label htmlFor="eventImage">Event image:</label>
-              <input
-                type="text"
-                id="eventImage"
-                onChange={handleTextChange}
-                value={newEvent.eventImage}
-              />
-              <label htmlFor="eventType">Event type:</label>
-              <select id="eventType" onChange={handleSelectChange}>
-                <option value=""></option>
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-                <option value="Intramural Sport">Intramural Sport</option>
-                <option value="Watch Party">Watch Party</option>
-                <option value="wedding">Wedding</option>
-              </select>
-              <br />
-              <input type="submit" />
-            </form>
+            <NewEventForm handleAddEvent={ handleAddEvent } newEvent={ newEvent } setNewEvent={ setNewEvent }/>
           </>
         </div>
         <div className="events">
@@ -161,39 +85,7 @@ function App() {
                         <div className="attendees">
                           {attendees.map((attendee, index) => (
                             <>
-                              <div key={attendee.id} className="attendee">
-                                <p>
-                                  <img
-                                    src={attendee.avatar}
-                                    alt={attendee.firstName}
-                                  />
-                                  {"   "}
-                                  <span>
-                                    {" "}
-                                    {attendee.firstName} {attendee.lastName}{" "}
-                                  </span>
-                                </p>
-                                <p>
-                                  <button
-                                    className="clickable"
-                                    onClick={() =>
-                                      updateEventAttendance(
-                                        event.id,
-                                        attendee.id
-                                      )
-                                    }
-                                  >
-                                    Attending:
-                                  </button>
-                                  <span>
-                                    {attendee.attendance ? "✅" : "❌"}
-                                  </span>
-                                </p>
-
-                                <p>
-                                  <span>Note:</span> {attendee.note}
-                                </p>
-                              </div>
+                              <Attendee attendee={ attendee } index={ index } events={ events } setEvents={ setEvents } event={ event } />
                             </>
                           ))}
                         </div>
@@ -207,13 +99,7 @@ function App() {
         </div>
       </main>
       <>
-        <footer>
-          <ul>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Legal</li>
-          </ul>
-        </footer>
+        <Footer />
       </>
     </div>
   );
--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,3 +1,34 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({ events, setEvents, showAttendees, setShowAttendees, toggleEventAttendees }) {
+  return(
+    <div className="events">
+      <ul>
+        {events.map((event) => {
+          const { people: attendees } = event;
+
+          return (
+            <>
+              <li key={event.id}>
+                <img src={event.eventImage} alt={event.name} />
+                <h5>
+                  {event.name} {event.eventType}
+                </h5>
+                <br />
+                <span>Organized by: {event.organizer} </span>
+                <br />
+                <>
+                  <button onClick={toggleEventAttendees}>
+                    {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                  </button>
+
+                  {showAttendees ? ( <Attendees attandees={ attendees } events={ events } setEvents={ setEvents } event={ event }/> ) : null}
+                </>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
