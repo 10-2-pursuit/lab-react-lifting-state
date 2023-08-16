8307c4d3a18d5a9328dc183dbc739ff110fc7dd1
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-16 12:58:47 -0400

    Update attendees list.

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,29 @@
-export default function Attendees() {
-  return;
+import { useState } from "react";
+import Attendee from "./Attendee";
+
+export default function Attendees({ event, setEvents, events }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+  const toggleEventAttendees = () => {
+    setShowAttendees(!showAttendees);
+  };
+  return (
+    <>
+      <button onClick={toggleEventAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+
+      {showAttendees ? (
+        <div className="attendees">
+          {event.people.map((attendee) => (
+            <Attendee
+              attendee={attendee}
+              event={event}
+              setEvents={setEvents}
+              events={events}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
