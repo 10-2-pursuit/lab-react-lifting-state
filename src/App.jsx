9f4b67aa70579cda13553c1a4108f29368d46f92
Addis Jackson <addisjackson@pursuit.org>
2023-09-26 17:44:30 -0400

    finishes lab

--- src/App.jsx
+++ src/App.jsx
@@ -23,20 +23,21 @@ function App() {
   };
 
   // Function to update attendance status of a specific attendee within an event
-  const updateEventAttendance = (eventId, attendeeId) => {
-    setEvents((prevEvents) =>
-      prevEvents.map((event) => {
-        if (event.id === eventId) {
-          const attendees = event.people
-          const updatedAttendees = attendees.map((attendee) =>
-            attendee.id === attendeeId ? { ...attendee, attendance: !attendee.attendance } : attendee
-          );
-          return { ...event, attendees: updatedAttendees };
-        }
-        return event;
-      })
+
+  function updateEventAttendance(eventId, attendeeId) {
+    const eventArray = [...events];
+    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
+    const event = { ...eventArray[eventIndex] };
+    const personIndex = event.people.findIndex(
+      (person) => person.id === attendeeId
     );
-  };
+    const peopleArray = [...event.people];
+    peopleArray[personIndex].attendance = !peopleArray[personIndex].attendance;
+    event.people = peopleArray;
+    eventArray[eventIndex] = event;
+    setEvents(eventArray);
+  }
+  
 
   return (
     <div className="App">
